607bcd09e65475007bb121db2e8e61b00530a6a7
fumtas1k <fumta01a7@gmail.com>
2022-01-27 21:31:23 +0900

    add:rails db:migrateを実施

--- db/schema.rb
+++ db/schema.rb
@@ -1,16 +1,14 @@
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
-# be faster and is potentially less error prone than running all of your
-# migrations from scratch. Old migrations may fail to apply correctly if those
-# migrations use external dependencies or application code.
-#
-# It's strongly recommended that you check this file into your version control system.
+ActiveRecord::Schema.define(version: 2022_01_27_120730) do
 
-ActiveRecord::Schema.define(version: 2022_01_27_115734) do
+  create_table "nearest_stations", force: :cascade do |t|
+    t.string "line_name"
+    t.string "name"
+    t.integer "walking_time"
+    t.integer "property_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["property_id"], name: "index_nearest_stations_on_property_id"
+  end
 
   create_table "properties", force: :cascade do |t|
     t.string "name", null: false
@@ -22,4 +20,5 @@ ActiveRecord::Schema.define(version: 2022_01_27_115734) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "nearest_stations", "properties"
 end
