8ce911809e3bc9682462246a359cf49b95afb545
fumtas1k <fumta01a7@gmail.com>
2022-01-27 21:57:22 +0900

    improve:propertiesビューからnearest_stationsの登録・編集ができるよう設定

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,25 +1,22 @@
 class PropertiesController < ApplicationController
   before_action :set_property, only: %i[ show edit update destroy ]
 
-  # GET /properties or /properties.json
   def index
     @properties = Property.all
   end
 
-  # GET /properties/1 or /properties/1.json
   def show
   end
 
-  # GET /properties/new
   def new
     @property = Property.new
+    2.times { @property.nearest_stations.build }
   end
 
-  # GET /properties/1/edit
   def edit
+    @property.nearest_stations.build
   end
 
-  # POST /properties or /properties.json
   def create
     @property = Property.new(property_params)
 
@@ -34,7 +31,6 @@ class PropertiesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /properties/1 or /properties/1.json
   def update
     respond_to do |format|
       if @property.update(property_params)
@@ -47,7 +43,6 @@ class PropertiesController < ApplicationController
     end
   end
 
-  # DELETE /properties/1 or /properties/1.json
   def destroy
     @property.destroy
     respond_to do |format|
@@ -57,13 +52,17 @@ class PropertiesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_property
-      @property = Property.find(params[:id])
-    end
+  def set_property
+    @property = Property.find(params[:id])
+  end
 
-    # Only allow a list of trusted parameters through.
-    def property_params
-      params.require(:property).permit(:name, :rent, :address, :age, :note)
-    end
+  def property_params
+    params.require(:property).permit(:name,
+      :rent,
+      :address,
+      :age,
+      :note,
+      nearest_stations_attributes: %i[ line_name name walking_time id property_id _destroy ],
+    )
+  end
 end
